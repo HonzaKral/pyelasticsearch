510a31f606447f6d4e9bae52fac17d635437fcf4
Honza Kral <honza.kral@gmail.com>
2013-07-31 18:06:24 +0200

    Removing unused exceptions

--- pyelasticsearch/exceptions.py
+++ pyelasticsearch/exceptions.py
@@ -1,5 +1,3 @@
-from requests import Timeout
-
 from elasticsearch import exceptions
 
 ConnectionError = exceptions.ConnectionError
@@ -9,14 +7,3 @@ ElasticHttpNotFoundError = exceptions.NotFoundError
 class IndexAlreadyExistsError(ElasticHttpError):
     """Exception raised on an attempt to create an index that already exists"""
 
-
-class InvalidJsonResponseError(Exception):
-    """
-    Exception raised in the unlikely event that ES returns a non-JSON response
-    """
-    @property
-    def response(self):
-        return self.args[0]
-
-    def __unicode__(self):
-        return u'Invalid JSON returned from ES: %r' % (self.response,)
